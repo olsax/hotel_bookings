1f00eb8a76daf5fd1be03eb529e69b3a4ae0896e
olsax <lolzzcake@gmail.com>
2020-10-22 00:08:21 +0200

    work on days between cancellation and arrival column

--- bookings.py
+++ bookings.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 pd.options.display.width = None
-pd.set_option('max_columns', 8)
+pd.set_option('max_columns', 10)
 plt.style.use('seaborn')
 
 # 'hotel', 'is_canceled' (1-cancelled), 'lead_time' (Number of days that elapsed between the entering date of the booking into the PMS and the arrival date),
@@ -25,7 +26,6 @@ plt.style.use('seaborn')
 # https://www.sciencedirect.com/science/article/pii/S2352340918315191
 
 dataf = pd.read_csv('hotel_bookings.csv')
-print(dataf.head())
 
 # numerical attributes
 #print(dataf.describe().columns)
@@ -59,10 +59,40 @@ dataf.drop(['is_repeated_guest', 'previous_bookings_not_canceled', 'required_car
 dataf['children'] = dataf['children'] + dataf['babies']
 dataf.drop(['babies'], axis=1, inplace=True)
 
-dataf['nights_in'] = dataf['stays_in_weekend_nights'] + dataf['stays_in_week_nights']
-dataf_corr = dataf.corr()
-print(dataf_corr['is_canceled'])
+dataf['stays_in_nights'] = dataf['stays_in_weekend_nights'] + dataf['stays_in_week_nights']
+dataf['arrival_date'] = dataf['arrival_date_day_of_month'].astype(str) + '-' + dataf['arrival_date_month'] + '-' + dataf['arrival_date_year'].astype(str)
+dataf['arrival_date'] = dataf['arrival_date'].apply(pd.to_datetime)
+dataf.drop(['arrival_date_day_of_month', 'arrival_date_week_number', 'arrival_date_month', 'arrival_date_year'], axis=1, inplace=True)
 
-sns.set_palette("pastel")
-sns.displot(dataf['lead_time'], bins=np.arange(0,dataf['lead_time'].max(),25), kde=False, height=4, aspect=2)
-plt.show()
+#print(dataf_corr['is_canceled'])
+
+print(dataf.head())
+
+##xx = [dataf['canc_to_arrival_days'] = (dataf['arrival_date'] - dataf['reservation_status_date']) if dataf['reservation_status'] == "Canceled" else (dataf['canc_to_arrival_days'] = -1) for row in dataf]
+
+#yy = [dataf['canc_to_arrival_days'] = (dataf['arrival_date'] - dataf['reservation_status_date']) if dataf['reservation_status'] == 'Canceled' for row in dataf]
+
+#dataf['reservation_status_date'] = datetime.strptime(dataf['reservation_status_date'], '%Y-%m-%d')
+dataf['reservation_status_date'] = pd.to_datetime(dataf['reservation_status_date'], format='%Y-%m-%d')
+#for row in dataf['reservation_status']:
+	#if row == 'Canceled':
+		#dataf['canc_to_arrival_days'] = (dataf['arrival_date'] - dataf['reservation_status_date'])
+		#print(dataf['canc_to_arrival_days'])
+	#else:
+	#	dataf['canc_to_arrival_days'] = 0
+print(dataf['canc_to_arrival_days'].describe())
+
+dataf['canc_to_arrival_days'] = np.where(dataf['reservation_status'] == 'Canceled', (dataf['arrival_date'] - dataf['reservation_status_date']), 0)
+#print(dataf['canc_to_arrival_days'])
+print(dataf['canc_to_arrival_days'].describe())
+
+#cancelled_data['canc_to_arrival_days'] = cancelled_data['arrival_date'] - cancelled_data['reservation_status_date']
+
+#sns.set_palette("pastel")
+#ax = sns.displot(dataf['lead_time'], bins=np.arange(0,dataf['lead_time'].max(),50), kde=False, height=4, aspect=2)
+#ax.set(xlabel="lead time to the arrival date")
+#plt.show()
+
+#sns.countplot(x='customer_type', data=dataf, hue='is_canceled')
+#plt.show()
+
